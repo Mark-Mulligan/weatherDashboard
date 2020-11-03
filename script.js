df9b9391033c4362435ff01b65396e1753911316
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2020-11-02 22:35:18 -0600

    added search history, organized javascript into functions

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let fiveDayForecasts = [];
+let citySearchHistory = [];
+let cityName = '';
+
+if (JSON.parse(localStorage.getItem('citySearchHistory') !== null)) {
+    console.log('search history exisits in local stoarge');
+    citySearchHistory = JSON.parse(localStorage.getItem('citySearchHistory'));
+    citySearchHistory.forEach(function (city) {
+        $('.search-list').append(`<div class="search-item">${city}</div>`);
+    })
+}
 
 $('#search-btn').click(function () {
     event.preventDefault();
@@ -6,130 +16,92 @@ $('#search-btn').click(function () {
     fiveDayForecasts = [];
 
     if ($('#cityNameInput').val() !== '') {
-        let cityName = $('#cityNameInput').val().trim();
-        $('#citynameInput').val('');
+        updateCitySearch();
 
-        const apiKey = "af81902a8a73c933aeffc3228ff6f7f1"
-        const units = "imperial"
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`
-
-        //`https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=af81902a8a73c933aeffc3228ff6f7f1&units=imperial`
+        const apiKey = "af81902a8a73c933aeffc3228ff6f7f1";
+        const units = "imperial";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
 
         $.ajax({
             url: url,
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            displayCurrentWeather(response);
 
-            $('.weather-info-container').removeClass('hidden');
-
-            let today = new Date();
-            let dd = today.getDate();
-            let mm = today.getMonth() + 1;
-            let yyyy = today.getFullYear();
-            if (dd < 10) {
-                dd = '0' + dd;
-            }
-            if (mm < 10) {
-                mm = '0' + mm;
-            }
-            //today = mm + '-' + dd + '-' + yyyy;
-            today = mm + '/' + dd + '/' + yyyy;
-        
-            $('#location').text(response.name + ' ' + today);
-            let icon = response.weather[0].icon;
-            $('#main-icon').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
-            $('#temperature').html('Temperature: ' + response.main.temp + ' &#176;F');
-            $('#humidity').text('Humidity: ' + response.main.humidity + '%');
-            $('#wind-speed').text('Wind Speed: ' + response.wind.speed + ' MPH');
-
-            let lat = response.coord.lat;
-            let lon = response.coord.lon;
-
-            let uviURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`
+            let uviURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${apiKey}`;
 
             $.ajax({
                 url: uviURL,
                 method: "GET"
             }).then(function (data) {
-                $('#uv-index').text('UV Index: ' + data.value);
-                
+                displayUVIndex(data);
             })
         });
 
-
-        const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=${units}`
+        const fiveDayURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=${units}`;
 
         $.ajax({
             url: fiveDayURL,
             method: "GET"
         }).then(function (data) {
-
-            $('.weekly-forecast-container').removeClass('hidden');
             let res = data.list;
             find3pmTimes(res);
-            let index = 0;
-
-            for (let i = 0; i < fiveDayForecasts.length; i++) {
-                index = fiveDayForecasts[i];
-
-                let card = $('<div>').addClass('card');
-                let cardBody1 = $('<div>').addClass('card-body');
-                cardBody1.addClass('border-bottom');
-                let dateEl = $('<h5>').text(formatDate(res[index].dt_txt));
-                let icon = res[index].weather[0].icon;
-                let iconEl = $('<img>').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
-                let iconWrapper = $('<div>').addClass('icon-wrapper');
-                //iconEl.addClass('img-fluid');
-                let cardBody2 = $('<div>').addClass('card-body');
-                cardBody2.addClass('border-top');
-                let tempEl = $('<p>').html('temp: ' + res[index].main.temp + ' &#176;F');
-                let humidityEl = $('<p>').text('humidity: ' + res[index].main.humidity);
-
-                cardBody1.append(dateEl);
-                iconWrapper.append(iconEl);
-                cardBody2.append(tempEl, humidityEl);
-                card.append(cardBody1, iconWrapper, cardBody2);
-
-                $('.five-day-container').append(card);
-            }
-
-
-
+            createForecastCards(res);
         })
-
-
-        /*
-        <div class="card" style="width: 17rem;">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                            </div>
-                            <img class="card-img-top" src="" alt="Card image cap">
-                            <div class="card-body">
-                                <p>Item 1</p>
-                                <p>Item 2</p>
-                            </div>
-                        </div>
-
-        */
-
-
-        //http://openweathermap.org/img/wn/10d@2x.png
-
-        //https://api.openweathermap.org/data/2.5/forecast?q=dallas&appid=af81902a8a73c933aeffc3228ff6f7f1&units=imperial`
-
-
-
-
-        /* 
-        date 
-        picture
-        temp 
-        humidity
-        */
-
     }
 })
+
+function updateCitySearch() {
+    cityName = $('#cityNameInput').val().trim();
+    citySearchHistory.unshift(cityName);
+    localStorage.setItem('citySearchHistory', JSON.stringify(citySearchHistory));
+    $('.search-list').prepend(`<div class="search-item">${cityName}</div>`);
+    $('#citynameInput').val('');
+}
+
+function createForecastCards(res) {
+    $('.weekly-forecast-container').removeClass('hidden');
+    let index = 0;
+
+    for (let i = 0; i < fiveDayForecasts.length; i++) {
+        index = fiveDayForecasts[i];
+        let card = $('<div>').addClass('card');
+        let cardBody1 = $('<div>').addClass('card-body border-bottom');
+        let dateEl = $('<h5>').text(formatDate(res[index].dt_txt));
+        let icon = res[index].weather[0].icon;
+        let iconEl = $('<img>').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
+        let iconWrapper = $('<div>').addClass('icon-wrapper');
+        let cardBody2 = $('<div>').addClass('card-body border-top');
+        let tempEl = $(`<p>Temp: <span>${res[index].main.temp} &#176;F</span><p>`);
+        let humidityEl = $(`<p>Humidity: <span>${res[index].main.humidity}</span></p>`);
+
+        cardBody1.append(dateEl);
+        iconWrapper.append(iconEl);
+        cardBody2.append(tempEl, humidityEl);
+        card.append(cardBody1, iconWrapper, cardBody2);
+        $('.five-day-container').append(card);
+    }
+}
+
+function displayCurrentWeather(response) {
+    $('.weather-info-container').removeClass('hidden');
+    $('#location').text(response.name + ' ' + getCurrentDate());
+    let icon = response.weather[0].icon;
+    $('#main-icon').attr('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
+    $('#temperature').html(response.main.temp + ' &#176;F');
+    $('#humidity').text(response.main.humidity + '%');
+    $('#wind-speed').text(response.wind.speed + ' MPH');
+}
+
+function displayUVIndex(data) {
+    $('#uv-index').html(`${data.value}`);
+    if (data.value > 10) $('#uv-index').addClass('extreme');
+    else if (data.value >= 8) $('#uv-index').addClass('very-high');
+    else if (data.value >= 6) $('#uv-index').addClass('high');
+    else if (data.value >= 3) $('#uv-index').addClass('moderate');
+    else $('#uv-index').addClass('favorable');
+}
 
 function find3pmTimes(array) {
     let timeRegex = /15:00:00/
@@ -144,9 +116,16 @@ function find3pmTimes(array) {
 function formatDate(str) {
     let year = str.slice(0, 4);
     let month = str.slice(5, 7);
-    let day = str.slice(8,10);
-    console.log(year);
-    console.log(month);
-    console.log(day);
+    let day = str.slice(8, 10);
     return `${month}/${day}/${year}`;
+}
+
+function getCurrentDate() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+    if (dd < 10) dd = '0' + dd;
+    if (mm < 10) mm = '0' + mm;
+    return mm + '/' + dd + '/' + yyyy;
 }
